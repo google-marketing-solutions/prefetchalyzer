b39fe22d24cec6316b95051e3e031f2b77c8862b
Stephan Giesau <giesau@google.com>
2020-09-17 10:50:58 +0200

    Add better styling for file upload form
    GitOrigin-RevId: 62d260801ef17310326c43c8f8384adecc928eae

--- src/components/file-upload.vue
+++ src/components/file-upload.vue
@@ -1,12 +1,57 @@
 <template>
-  <div>
-    <h1 class="mdc-typography--headline3"><span class="logo--emphasized">Prefetch</span>alyzer</h1>
-    <p class="mdc-typography--body1">Record a user session in HAR format, and upload it here to identify prefetching opportunities.</p>
-    <textarea v-model.lazy="harTextInput"></textarea>
-    <button @click="processTextInput()">Parse text input</button>
-    –OR– Select a file here:
-    <input type="file" @change="processFile" accept=".har" />
-    <span v-show="uploadProgress !== null">Loading file ... {{ uploadProgress }}%</span>
+  <div class="file-upload-container">
+    <div class="file-upload-container__inner">
+      <h1 class="mdc-typography--headline4">
+        <span class="logo--emphasized">Prefetch</span>alyzer
+      </h1>
+      <p
+        class="mdc-typography--body1"
+      >Record a user session in HAR format, and upload it here to identify prefetching opportunities.</p>
+
+      <input class="mdc-button mdc-button--raised" type="file" @change="processFile" accept=".har" />
+      <p
+        class="mdc-typography--body1"
+        v-show="state.uploadProgress !== null"
+      >Loading file ... {{ state.uploadProgress }}%</p>
+
+      <p>
+        <button
+          v-show="!state.showTextInput"
+          @click="state.showTextInput = true"
+          class="mdc-button"
+        >
+          <span class="mdc-button__label">Input JSON as text</span>
+        </button>
+      </p>
+
+      <div v-show="state.showTextInput">
+        <hr />
+        <label
+          class="mdc-text-field mdc-text-field--outlined mdc-text-field--textarea mdc-text-field--no-label"
+        >
+          <span class="mdc-text-field__resizer">
+            <textarea
+              v-model.lazy="harTextInput"
+              class="mdc-text-field__input"
+              rows="4"
+              cols="40"
+              aria-label="Label"
+              placeholder="Insert HAR JSON here ..."
+            ></textarea>
+          </span>
+          <span class="mdc-notched-outline">
+            <span class="mdc-notched-outline__leading"></span>
+            <span class="mdc-notched-outline__trailing"></span>
+          </span>
+        </label>
+        <br />
+        <p>
+          <button class="mdc-button mdc-button--raised" @click="processTextInput()">
+            <span class="mdc-button__label">Parse text input</span>
+          </button>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,14 +61,22 @@ import { parseHARFile } from '@/utils/har-utils'
 
 @Component
 export default class FileUpload extends Vue {
-  // holds current progress of file upload or 'null' when finished
-  private uploadProgress: number | null = null;
+  private state: { uploadProgress: number | null; showTextInput: boolean } = {
+    // holds current progress of file upload or 'null' when finished
+    uploadProgress: null,
+    showTextInput: false
+  }
+
   // holds HAR input from textarea
-  private harTextInput = '';
+  private harTextInput = ''
   // holds file contents from HAR file upload
-  private harFile = '';
+  private harFile = ''
 
   processTextInput() {
+    if (!this.harTextInput.length) {
+      return
+    }
+
     this.$emit('updateParsedHAR', parseHARFile(this.harTextInput))
   }
 
@@ -39,7 +92,7 @@ export default class FileUpload extends Vue {
 
     const reader = new FileReader()
     reader.addEventListener('load', (event) => {
-      this.uploadProgress = null
+      this.state.uploadProgress = null
       // TODO: could also be ArrayBuffer type. Add type check
       this.harFile = (event?.target?.result ? event.target.result : '') as string
       this.$emit('updateParsedHAR', parseHARFile(this.harFile))
@@ -47,7 +100,7 @@ export default class FileUpload extends Vue {
     reader.addEventListener('progress', (event) => {
       if (event.loaded && event.total) {
         const percent = (event.loaded / event.total) * 100
-        this.uploadProgress = percent
+        this.state.uploadProgress = percent
       }
     })
     reader.readAsText(file)
@@ -57,6 +110,20 @@ export default class FileUpload extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@use "@material/button/mdc-button";
+@use "@material/textfield/mdc-text-field";
+
+.file-upload-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__inner {
+    max-width: 40rem;
+    text-align: center;
+  }
+}
+
 .logo--emphasized {
   color: var(--mdc-theme-primary);
 }
